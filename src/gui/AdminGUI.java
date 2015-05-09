3951367e24018c75b40c1ffe46cf20cae1fbc94b
Piotr Górak <piotrgorak93@gmail.com>
2015-05-09 18:09:45 +0200

    Initial commit GUI

---
+++ src/gui/AdminGUI.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Piotr Górak, Maciej Knicha³ dnia 2015-05-09.
+ */
+public class AdminGUI extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
