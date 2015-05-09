922883ba9a60ddc5c8e0bc1ce3004284545b5dac
Piotr Górak <piotrgorak93@gmail.com>
2015-05-09 15:19:40 +0200

    Initial commit
    Login works

---
+++ src/gui/User.java
@@ -0,0 +1,14 @@
+package gui;
+
+/**
+ * @author Piotr Górak, Maciej Knicha³ dnia 2015-05-04.
+ */
+public class User {
+    String nazwa;
+    String haslo;
+
+    public User(String nazwa, String haslo) {
+        this.nazwa = nazwa;
+        this.haslo = haslo;
+    }
+}
